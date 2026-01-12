d36ee95eb59bea892e19780e1ee1e1bbc404d2ce
animeshjojo <animeshmdp@gmail.com>
2026-01-12 14:58:59 +0530

    Added shortUrldto

--- src/main/java/com/example/url_shortner/Controller/PublicController.java
+++ src/main/java/com/example/url_shortner/Controller/PublicController.java
@@ -1,12 +1,14 @@
 package com.example.url_shortner.Controller;
 
 import com.example.url_shortner.Dto.LongUrlDto;
+import com.example.url_shortner.Dto.ShortUrlDto;
 import com.example.url_shortner.Services.PublicService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class PublicController {
 
    private PublicService publicService;
@@ -16,13 +18,14 @@ public class PublicController {
    }
 
    @PostMapping
-   public ResponseEntity<String> saveData(@RequestBody LongUrlDto longUrlDto) {
+   public ResponseEntity<?> saveData(@RequestBody LongUrlDto longUrlDto) {
        try{
            if(longUrlDto.getLongUrl()==null || longUrlDto.getLongUrl().isEmpty()){
                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("LongUrl is empty");
            }
-           String shorturl=publicService.save(longUrlDto);
-           return ResponseEntity.status(HttpStatus.CREATED).body("http://localhost:8080/"+shorturl);
+           ShortUrlDto shortUrlDto = new ShortUrlDto();
+           shortUrlDto.setShortUrl("http://localhost:8080/"+publicService.save(longUrlDto));
+           return ResponseEntity.status(HttpStatus.CREATED).body(shortUrlDto);
        }
        catch(Exception e){
            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Please Try again");
---
+++ src/main/java/com/example/url_shortner/Dto/ShortUrlDto.java
@@ -0,0 +1,9 @@
+package com.example.url_shortner.Dto;
+
+import lombok.Data;
+
+@Data
+public class ShortUrlDto {
+
+    private String shortUrl;
+}
