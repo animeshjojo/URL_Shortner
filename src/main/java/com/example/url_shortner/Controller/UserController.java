0b9b3c23a2199c4d18a5f0f60158db65bbb1ca46
animeshjojo <animeshmdp@gmail.com>
2026-01-21 21:33:54 +0530

    Added Jwt filter implementation and a feature where authenticated user can fetch all their url data

--- src/main/java/com/example/url_shortner/Controller/UserController.java
+++ src/main/java/com/example/url_shortner/Controller/UserController.java
@@ -1,11 +1,25 @@
 package com.example.url_shortner.Controller;
 
+import com.example.url_shortner.Entity.UrlData;
+import com.example.url_shortner.Services.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private UserService userService;
+
+    @GetMapping
+    public List<UrlData>  getUrlData(){
+        String username= SecurityContextHolder.getContext().getAuthentication().getName();
+        return userService.getUrlDataByUser(username);
+    }
 
 
 }
--- src/main/java/com/example/url_shortner/Services/UserService.java
+++ src/main/java/com/example/url_shortner/Services/UserService.java
@@ -1,5 +1,6 @@
 package com.example.url_shortner.Services;
 
+import com.example.url_shortner.Entity.UrlData;
 import com.example.url_shortner.Entity.User;
 import com.example.url_shortner.Repository.UserRepositry;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,9 @@ public class UserService {
 
     public List<User> getAllUsers() {
         return userRepositry.findAll();
+    }
+
+    public List<UrlData> getUrlDataByUser(String username) {
+        return userRepositry.findUserByUserName(username).getUrls();
     }
 }
